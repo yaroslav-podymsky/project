e85c9fedbd1c75c31b6bde8bcfdfa11a0fa04a12
yaroslav <jarik9623@mail.ru>
2021-05-04 10:55:42 +0300

    Components comit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import Profile from './components/Profile'
+
 
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src='https://w7.pngwing.com/pngs/210/622/png-transparent-indonesian-ministry-of-labour-government-ministries-of-indonesia-workforce-logo-indonesia-miscellaneous-hand-logo.png' />
-      </header>
-      <nav className='nav'>
-        <div> <a>Profile</a></div>
-        <div> <a>Messages</a></div>
-        <div> <a>News</a></div>
-        <div> <a>Music</a></div>
-        <div> <a>Settings</a></div>
-      </nav>
-      <div className='content'>Main content</div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>);
 }
 
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+    <img src='https://thumbs.dreamstime.com/b/%D0%B3%D0%BE%D0%BB%D1%83%D0%B1%D0%BE%D0%B5-%D0%BD%D0%B5%D0%B1%D0%BE-%D0%B7%D0%B5%D0%BB%D0%B5%D0%BD%D0%BE%D0%B3%D0%BE-%D1%86%D0%B2%D0%B5%D1%82%D0%B0-%D1%82%D1%80%D0%B0%D0%B2%D1%8B-21428173.jpg' />
+    </div>
+    <div>ava+description</div>
+    <div>my posts
+      <div>new post</div>
+      <div>
+        <div>post-1</div>
+        <div>post-2</div>
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile;
